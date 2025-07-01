15d884a9fde8fecafd210616d4e817a267fd1bc8
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2025-07-01 20:49:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Install MediaPipe from source
 git clone https://github.com/google/mediapipe.git
 cd mediapipe
 python setup.py install
+cd ..
+rm -rf mediapipe  # Clean up after installation

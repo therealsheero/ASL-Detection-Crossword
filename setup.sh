3b976ddeed50ac272ee813a8f8073dbef664c3fb
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2025-07-01 20:58:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-!/bin/bash
- -*- mode: shell-script -*-
+#!/bin/bash
+#-*- mode: shell-script -*-
 
 # Install MediaPipe with explicit Python 3.12 wheel
 pip install https://files.pythonhosted.org/packages/cp312/m/mediapipe/mediapipe-0.10.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
